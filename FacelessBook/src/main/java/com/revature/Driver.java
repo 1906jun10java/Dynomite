c16b9967e022dd6c21ac54d7003960c97fee2d9c
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-08-03 21:30:29 -0400

    Finalized authentication Dao

--- FacelessBook/src/main/java/com/revature/Driver.java
+++ FacelessBook/src/main/java/com/revature/Driver.java
@@ -6,20 +6,28 @@ import org.hibernate.Transaction;
 
 import com.revature.beans.Credentials;
 import com.revature.beans.Users;
+import com.revature.daoimpl.UsersDaoImpl;
 import com.revature.util.ConnectionUtil;
 
 public class Driver {
 	
+	static private UsersDaoImpl udi = new UsersDaoImpl();
+	
 	public static void main(String[] args) {
-		SessionFactory sf = ConnectionUtil.getSessionFactory();
+		//SessionFactory sf = ConnectionUtil.getSessionFactory();
 
-		Session s = sf.openSession(); 
-		Transaction tx = s.beginTransaction();
-		Credentials creds = s.get(Credentials.class, "Goombazio");
-		s.save(new Users("Richard", "DiCosmo", creds, 1, 1, "rjdicosmo@gmail.com"));
+		//Session s = sf.openSession(); 
+		//Transaction tx = s.beginTransaction();
+		//Credentials creds = s.get(Credentials.class, "Goombazio");
+		//s.save(new Users("Richard", "DiCosmo", creds, 1, 1, "rjdicosmo@gmail.com"));
+		
+		Credentials creds = new Credentials("Goombazio", "pass");
+		Users loggedIn = udi.authenticateUser(creds);
+		System.out.println(loggedIn);
+		
 
 		
-		tx.commit(); 
-		s.close();
+		//tx.commit(); 
+		//s.close();
 	}
 }
--- FacelessBook/src/main/java/com/revature/daoimpl/UsersDaoImpl.java
+++ FacelessBook/src/main/java/com/revature/daoimpl/UsersDaoImpl.java
@@ -5,33 +5,50 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.beans.Credentials;
 import com.revature.beans.Users;
 import com.revature.dao.UsersDao;
+import com.revature.util.ConnectionUtil;
 
 @Repository(value="UsersDAO")
 @Transactional
 public class UsersDaoImpl implements UsersDao{
 
-	private SessionFactory sessionFactory;
+	private SessionFactory sf = ConnectionUtil.getSessionFactory();
 	
 	@Override
 	public Users authenticateUser(Credentials credentials) {
-		String sql = "SELECT * FROM USERS WHERE USERNAME IN (SELECT USERNAME FROM CREDENTIALS WHERE USERNAME="+credentials.getUsername()+" AND PASS="+ credentials.getPass();
-		Query<Users> query = sessionFactory.getCurrentSession().createSQLQuery(sql);
-		List<Users> lu = query.getResultList();
-		Users u = lu.get(0);
+		Users u = null;
+		//String sql = "SELECT * FROM USERS WHERE USERNAME IN (SELECT USERNAME FROM CREDENTIALS WHERE USERNAME="+credentials.getUsername()+" AND PASS="+ credentials.getPass();
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		Credentials creds = s.load(Credentials.class, credentials.getUsername());
+		System.out.println(creds);
+		if(creds.getPass().equals(credentials.getPass())) {
+			String hql = "FROM Users WHERE USERNAME='" + credentials.getUsername() + "'";
+			Query query = s.createQuery(hql);
+			List results = query.list();
+			u = (Users) results.get(0);
+		}
+		else {
+			System.out.println("COULD NOT FIND MATCHING USER");
+		}
+		s.close();
 		return u;
 	}
 
 	@Override
 	public boolean createUser(Users user) {
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
 		try {
-			sessionFactory.getCurrentSession().persist(user);
+			s.persist(user);
 			return true;
 		}catch(Exception e) {
 			e.printStackTrace();
