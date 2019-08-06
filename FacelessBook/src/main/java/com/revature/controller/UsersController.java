812df87a7a96f6c6f934ca47f30f8c1082522b3c
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-08-05 22:33:07 -0400

    Changed UserController

--- FacelessBook/src/main/java/com/revature/controller/UsersController.java
+++ FacelessBook/src/main/java/com/revature/controller/UsersController.java
@@ -2,14 +2,12 @@ package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Credentials;
@@ -29,14 +27,15 @@ public class UsersController {
 		this.userService = userService;
 	}
 	
-	/*
+	
 	@ResponseStatus(HttpStatus.OK)
 	@PostMapping(value="/login", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public Users getCredentials(@RequestBody Credentials credentials) {
 		return userService.authenticateUser(credentials);
 	}
-	*/
 	
+	
+	/*
 	@RequestMapping(value="/{username}/{pass}", method=RequestMethod.POST)
 	public ResponseEntity<Users> getUserByUsername(@PathVariable String username, @PathVariable String pass) {
 		Credentials creds = new Credentials(username, pass);
@@ -47,6 +46,6 @@ public class UsersController {
 			return new ResponseEntity<>(f, HttpStatus.OK);
 		}
 	}
-	
+	*/
 
 } 
